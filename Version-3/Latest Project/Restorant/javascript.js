686fb0be29c01a28e100aebe9a6558b3fbf7ed6f
Arslan <140950959+Arslanasif9t9@users.noreply.github.com>
2025-05-21 02:00:16 +0500

    Some changing

---
+++ Version-3/Latest Project/Restorant/javascript.js
@@ -0,0 +1,10 @@
+console.log('Project');
+
+const home = document.getElementById('home');
+
+console.log(home.offsetTop);
+console.log(home.offsetLeft);
+
+function scroll(element){
+    console.log(element);
+}
